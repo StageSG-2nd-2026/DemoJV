83b73cc855f5eb7109cb0fa58892f41d08f1fee6
Valentin Victoor <valentin.victoor@gmail.com>
2026-06-17 10:26:17 +0200

    Add ScoreManager and EnemySpawner classes

---
+++ managers.py
@@ -0,0 +1,48 @@
+import time
+
+class ScoreManager:
+    def __init__(self):
+        self.start_time = time.time()
+
+    def get_elapsed_time(self):
+        return time.time() - self.start_time
+
+    def calculate_final_score(self, player, elapsed):
+        score = player.score
+
+        if player.hp == 250:
+            score += 1000
+        elif player.hp >= 200:
+            score += 500
+        elif player.hp >= 150:
+            score += 250
+        elif player.hp >= 100:
+            score += 100
+        elif player.hp >= 50:
+            score += 50
+
+        if elapsed < 60:
+            score += 1000
+        elif elapsed < 90:
+            score += 500
+
+        return score
+
+    def get_rank(self, score):
+        if score >= 5000:
+            return "S"
+        elif score >= 3500:
+            return "A"
+        elif score >= 2500:
+            return "B"
+        elif score >= 1500:
+            return "C"
+        return "D"
+
+
+class EnemySpawner:
+    def __init__(self):
+        self.enemies = []
+
+    def spawn_enemy(self):
+        pass
